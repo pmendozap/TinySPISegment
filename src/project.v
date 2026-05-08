d460c7b7eb887d825aea3865a1e62b27768f697b
Pablo Mendoza Ponce <77683977+pmendozap@users.noreply.github.com>
2026-05-07 18:48:13 -0600

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,4 +1,4 @@
-module tt_um_pmendozap_tiny_spi_segment (
+module tt_um_pmendozap_ieee_tiny_spi_segment (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
